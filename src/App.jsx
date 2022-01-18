4b40029a01b9f9417a45154528123739ed8bfd55
danieldamian09 <humbertorivero9@gmail.com>
2022-01-18 14:26:33 -0300

    add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+`
 
 function App() {
 
   return (
-    <h1>Desde App</h1>
+    <Heading>Desde App</Heading>
   )
 }
 
