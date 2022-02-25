5cb3b2668df66f161233c625de02aec8b95c3a43
danieldamian09 <humbertorivero9@gmail.com>
2022-02-25 17:37:19 -0300

    create custom hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import styled from "@emotion/styled"
-import useSelectMoneda from "../hooks/useSelectMoneda"
+import React from "react";
+import styled from "@emotion/styled";
+import useSelectMoneda from "../hooks/useSelectMoneda";
+import { monedas } from "../data/monedas";
 
 const InputSubmit = styled.input`
-  background-color: #9497ff;
-  border: none;
-  width: 100%;
-  padding: 10px;
-  color: #fff;
-  font-weight: 700;
-  text-transform: uppercase;
-  font-size: 20px;
-  border-radius: 5px;
-  transition: background-color .3s ease;
+	background-color: #9497ff;
+	border: none;
+	width: 100%;
+	padding: 10px;
+	color: #fff;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 20px;
+	border-radius: 5px;
+	transition: background-color 0.3s ease;
+  margin-top: 30px;
 
-    &:hover{
-      background-color: #7A7DFE;
-      cursor: pointer;
-    }
-`
+	&:hover {
+		background-color: #7a7dfe;
+		cursor: pointer;
+	}
+`;
 
 const Formulario = () => {
+	const [SelectMonedas] = useSelectMoneda("Elige tu Moneda", monedas);
 
-  const [ SelectMonedas ] = useSelectMoneda("Elige tu Moneda")
+	return (
+		<form>
+			<SelectMonedas />
+			<InputSubmit type="submit" value="Cotizar" />
+		</form>
+	);
+};
 
-  
-  return (
-    <form>
-      <SelectMonedas />
-      <InputSubmit type="submit" value="Cotizar" />
-    </form>
-  )
-}
-
-export default Formulario
+export default Formulario;
--- src/hooks/useSelectMoneda.jsx
+++ src/hooks/useSelectMoneda.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import styled from "@emotion/styled"
+import {useState} from "react";
+import styled from "@emotion/styled";
 
 const Label = styled.label`
-  color: #fff;
-`
+	color: #fff;
+	display: block;
+	font-family: "Lato", sans-serif;
+	font-size: 24px;
+	font-weight: 700;
+	margin: 15px 0;
+`;
 
-const useSelectMoneda = (label) => {
-  
-  const Selectmonedas = () => (
-    <>
-      <Label>{label}</Label>
-    </>
-  )
+const Select = styled.select`
+	width: 100%;
+	padding: 14px;
+	border-radius: 10px;
+`;
 
-  return [Selectmonedas]
-}
+const useSelectMoneda = (label, opciones) => {
+	const [state, setState] = useState("");
 
-export default useSelectMoneda
+	const Selectmonedas = () => (
+		<>
+			<Label>{label}</Label>
+			<Select value={state} onChange={ e => setState(e.target.value)}>
+				<option value="">Seleccione</option>
+				{opciones.map((opcion) => (
+					<option key={opcion.id} value={opcion.id}>
+						{opcion.nombre}
+					</option>
+				))}
+			</Select>
+		</>
+	);
+
+	return [Selectmonedas];
+};
+
+export default useSelectMoneda;
