9def117a311ff5c0823427563d530d44494e3ced
danieldamian09 <humbertorivero9@gmail.com>
2022-03-15 19:17:55 -0300

    creating an array with API cryptos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import styled from "@emotion/styled";
 import useSelectMoneda from "../hooks/useSelectMoneda";
-import { monedas } from "../data/monedas";
+import {monedas} from "../data/monedas";
 
 const InputSubmit = styled.input`
 	background-color: #9497ff;
@@ -14,7 +14,7 @@ const InputSubmit = styled.input`
 	font-size: 20px;
 	border-radius: 5px;
 	transition: background-color 0.3s ease;
-  margin-top: 30px;
+	margin-top: 30px;
 
 	&:hover {
 		background-color: #7a7dfe;
@@ -23,25 +23,40 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
+
+	const [criptos, setCriptos] = useState([])
 	const [moneda, SelectMonedas] = useSelectMoneda("Elige tu Moneda", monedas);
 
 	// llamada a la API
-	const consultarAPI = async() => {
-		const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`
-		const respuesta = await fetch(url)
-		const resultado = await respuesta.json()
+	const consultarAPI = async () => {
+		try {
+			const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
+			const respuesta = await fetch(url);
+			const {Data} = await respuesta.json();
 
-		console.log(resultado.Data)
-	}
+			const arrayCriptos = Data.map((criptos) => {
+				const objetoCriptos = {
+					id: criptos.CoinInfo.Name,
+					nombre: criptos.CoinInfo.FullName,
+				};
+
+				return objetoCriptos;
+			});
+
+			setCriptos(arrayCriptos);
+		} catch (error) {
+			return error;
+		}
+	};
 
 	useEffect(() => {
-		consultarAPI()
-	},[])
+		consultarAPI();
+	}, []);
 
 	return (
 		<form>
 			<SelectMonedas />
-      {moneda}
+			{moneda}
 			<InputSubmit type="submit" value="Cotizar" />
 		</form>
 	);
