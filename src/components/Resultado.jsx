5e1b8f0d874312139d3f98e8e071d783238624ae
danieldamian09 <humbertorivero9@gmail.com>
2022-03-16 17:14:21 -0300

    styles for the quote result

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
+import styled from "@emotion/styled";
 
-const Resultado = ({ cotizacion }) => {
-  
-  const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = cotizacion
+const Container = styled.div`
+	color: #fff;
+	font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
 
-  return (
-    <div>
-      <p>El precio es de: <span>{PRICE}</span></p>
-      <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-      <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-      <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Última actualización: <span>{LASTUPDATE}</span></p>
-    </div>
-  )
-}
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`
 
-export default Resultado
+const Texto = styled.p`
+	font-size: 18px;
+	span {
+		font-weight: 700;
+	}
+`;
+
+const Precio = styled.p`
+	font-size: 24px;
+	span {
+		font-weight: 700;
+	}
+`;
+
+const Resultado = ({cotizacion}) => {
+	const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} =
+		cotizacion;
+
+	return (
+		<Container>
+			<Imagen
+				src={`https://cryptocompare.com/${IMAGEURL}`}
+				alt="Imagen Criptomoneda"
+			/>
+			<div>
+				<Precio>
+					El precio es de: <span>{PRICE}</span>
+				</Precio>
+				<Texto>
+					Precio más alto del día: <span>{HIGHDAY}</span>
+				</Texto>
+				<Texto>
+					Precio más bajo del día: <span>{LOWDAY}</span>
+				</Texto>
+				<Texto>
+					Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+				</Texto>
+				<Texto>
+					Última actualización: <span>{LASTUPDATE}</span>
+				</Texto>
+			</div>
+		</Container>
+	);
+};
+
+export default Resultado;
